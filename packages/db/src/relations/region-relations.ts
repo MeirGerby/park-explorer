2a68e14fe48c3c4511a8ddc5e1ca0731b1c1f8c3
MEIRG <meirg2001@gmail.com>
2026-08-11 18:03:46 +0300

    refactor: delete relations files and units them into one file

--- packages/db/src/relations/region-relations.ts
+++
@@ -1,6 +0,0 @@
-import { relations } from 'drizzle-orm'
-import { cities, regions } from '../index'
-
-export const regionRelations = relations(regions, ({ many }) => ({
-    cities: many(cities)
-}))
--- packages/db/src/relations/user-relations.ts
+++
@@ -1,9 +0,0 @@
-
-import { relations } from 'drizzle-orm'
-
-import { parks, users } from '../index'
-
-
-export const userRelations = relations(users, ({ many }) => ({
-    parks: many(parks)
-}))
