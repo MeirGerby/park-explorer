be2243c82815d522ae07c6ea59b0b9f9396f5bfd
MEIRG <meirg2001@gmail.com>
2026-08-11 12:49:01 +0300

    chore(drizzle): add drizzle config file

---
+++ packages/db/drizzle.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'drizzle-kit'
+import * as dotenv from 'dotenv'
+
+dotenv.config({path: '../../.env'})
+
+if (!process.env.DATABASE_URL) {
+    throw new Error('DATABASE_URL environment variable is missing')
+}
+
+export default defineConfig({
+    dialect: 'postgresql',
+    schema: './src/schema/index.ts',
+    out: './drizzle/migrations',
+    dbCredentials: {
+        url: process.env.DATABASE_URL
+    }
+})
+
