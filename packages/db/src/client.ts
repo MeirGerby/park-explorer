5b1c6191ba333553cc16262df073963c0e0d5cf8
MEIRG <meirg2001@gmail.com>
2026-08-11 14:33:31 +0300

    fix: change the client connection

--- packages/db/src/client.ts
+++ packages/db/src/client.ts
@@ -1,10 +1,20 @@
 import { Pool } from "@neondatabase/serverless";
-import { drizzle } from "drizzle-orm/neon-serverless"
-import * as schema from './index'
+import { drizzle } from "drizzle-orm/neon-serverless";
 
 export function createDatabaseClient(connectionString: string) {
-    const pool = new Pool({ connectionString })
-    return drizzle(pool, { schema})
+  const pool = new Pool({
+    connectionString,
+  });
+
+  const db = drizzle({
+    client: pool,
+  });
+
+  return {
+    db,
+    pool,
+  };
 }
 
-export type Database = ReturnType<typeof createDatabaseClient>
+export type Database = ReturnType<typeof createDatabaseClient>["db"];
+export type DatabasePool = ReturnType<typeof createDatabaseClient>["pool"];
