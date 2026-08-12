084fe99fb20797df9e70d339fc34cfd98be0c090
MEIRG <meirg2001@gmail.com>
2026-08-12 11:28:26 +0300

    fix: add missing column name

--- packages/db/src/schema/regions.ts
+++ packages/db/src/schema/regions.ts
@@ -3,5 +3,5 @@ import { pgTable, text, uuid } from "drizzle-orm/pg-core"
 export const regions = pgTable('regions', {
     id: uuid('id').defaultRandom().primaryKey(),
 
-    name: text().notNull(),
+    name: text('name').notNull(),
 })
