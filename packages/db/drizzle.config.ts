0e621505b065af51e8ba5bdb8db5ebdc60b47427
MEIRG <meirg2001@gmail.com>
2026-08-12 11:39:58 +0300

    refactor(drizzle): change the migration out folder

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -10,7 +10,7 @@ if (!process.env.DATABASE_URL) {
 export default defineConfig({
     dialect: 'postgresql',
     schema: './src/schema/index.ts',
-    out: './drizzle/migrations',
+    out: './drizzle',
     dbCredentials: {
         url: process.env.DATABASE_URL
     }
