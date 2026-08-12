0e6b6e53b6f4c300d884e96e2d2ca0613bee4366
MEIRG <meirg2001@gmail.com>
2026-08-12 18:25:14 +0300

    refactor(api): remove hardcoded url

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,11 +1,11 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './app/app.module.js';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: process.env.CORS_ORIGIN ?? 'http://localhost:5173',
     credentials: true,
   });
 
