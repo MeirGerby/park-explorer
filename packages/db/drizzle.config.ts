549f992d4ad78ac1356b79afd5ef7235067f4ae0
MEIRG <meirg2001@gmail.com>
2026-08-16 10:14:43 +0300

    chore: migration files

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -9,7 +9,7 @@ if (!process.env.DATABASE_URL) {
 
 export default defineConfig({
     dialect: 'postgresql',
-    schema: './src/schema/index.ts',
+    schema: './dist/schema/index.js',
     out: './drizzle',
     dbCredentials: {
         url: process.env.DATABASE_URL
