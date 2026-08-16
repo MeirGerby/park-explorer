8637721bef02c4b0523c1520db57484340af5c80
MEIRG <meirg2001@gmail.com>
2026-08-16 15:19:31 +0300

    refactor: add password field to users schema

--- packages/db/src/schema/users.ts
+++ packages/db/src/schema/users.ts
@@ -4,6 +4,8 @@ export const users = pgTable("users", {
     id: uuid("id").defaultRandom().primaryKey(),
 
     name: text("name").notNull(),
-    
-    email: text("email").notNull().unique()
+
+    email: text("email").notNull().unique(),
+
+    passwordHash: text("password_hash").notNull()
 })
