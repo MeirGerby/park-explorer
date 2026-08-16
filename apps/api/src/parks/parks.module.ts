0b17eed3574f8f7c4b5451fb9579fb95db1b049c
MEIRG <meirg2001@gmail.com>
2026-08-16 15:21:40 +0300

    chore: parks migration files

--- apps/api/src/parks/parks.module.ts
+++ apps/api/src/parks/parks.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ParksRouter } from './parks.router.js';
 import { ParksService } from './parks.service.js';
+import { ParksRepository } from './parks.repository.js';
 
 @Module({
-  providers: [ParksRouter, ParksService],
+  providers: [ParksRouter, ParksService, ParksRepository],
 })
 export class ParksModule {}
